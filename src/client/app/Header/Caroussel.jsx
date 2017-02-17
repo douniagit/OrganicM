c0fec0f2dedd359f8f7f2ff522cb417f666b930f
dounia <d.fergane@gmail.com>
2017-02-17 17:53:11 +0100

    start navbar & create caroussel component

---
+++ src/client/app/Header/Caroussel.jsx
@@ -0,0 +1,17 @@
+import React from 'React';
+
+
+class Header extends React.Component{
+		render(){
+
+			return(
+				<div className="slide">
+				
+				</div>
+				
+			
+			);
+		}
+}
+
+export default Header;
--- src/client/app/Header/Header.jsx
+++ src/client/app/Header/Header.jsx
@@ -1,17 +1,33 @@
-import React, from "react";
+import React from 'React';
 
 
 class Header extends React.Component{
 		render(){
 
 			return(
-				<div className="container">
-					
-
-
-
+				<div className="header">
+					<div className="navpart1">
+						<ul>
+							<li>Home</li>
+							<li>Présentation</li>
+							<li>Disciplines
+								<ul>
+									<li>Nos disciplines</li>
+									<li>Activités pédagogiques</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+					<div className="logo"><img src="#"/></div>
+					<div className="navPart2">
+						<ul>
+							<li>Tarifs</li>
+							<li>Notre équipe</li>
+							<li>Nos partenaires</li>
+						</ul>
+					</div>
 				</div>
-			
+				
 			
 			);
 		}
