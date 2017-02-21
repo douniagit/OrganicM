de08cb08bfa89d0f0f416ab0f576833f979709d1
fousseni <foussenicoulibalypro@gmail.com>
2017-02-21 19:39:21 +0100

    add footer

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,12 +1,14 @@
 import React from 'React';
 import {render} from 'react-dom';
 import Header from './Header/Header.jsx';
+import Footer from './Footer/Footer.jsx';
 
 class App extends React.Component {
   render () {
     return (
       <div>
     	<Header />
+      <Footer />
       </div>
     );
   }
